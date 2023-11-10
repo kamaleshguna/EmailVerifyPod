b18bcb3fc5e15a95e75b3b0d371b167843710805
Kamalesh <kamalesh2002.gk@gmail.com>
2023-11-10 14:43:48 +0530

    changes

---
+++ DummyApps/DummyApps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DummyApps
+//
+//  Created by Closerlook on 11/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
---
+++ DummyApps/Pods/Target Support Files/Pods-DummyApps/Pods-DummyApps-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DummyApps : NSObject
+@end
+@implementation PodsDummy_Pods_DummyApps
+@end
